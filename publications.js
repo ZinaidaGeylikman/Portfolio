9db9b925021e1fe682137cf8056860a2997bfa01
Zinaida Geylikman <zinaida.geylikman@gmail.com>
2026-03-20 12:02:58 +0100

    Add EN/FR toggle, bilingual translations, badge and layout fixes

--- publications.js
+++ publications.js
@@ -24,19 +24,19 @@ const defaultPubs = [
   // DIRECTION
   { id:2, cat:'direction', authors:'Geylikman Z. & Lambert P. (éds.)', year:'2016', title:'Rythmes d\'évolution en français médiéval. Observations d\'après quelques textes littéraires', ref:'Paris, L\'Harmattan.', doi:'' },
   { id:3, cat:'direction', authors:'Boudes Y., Geylikman Z., Lambert P. & Sanchez A. (éds.)', year:'2019', title:'Rythmes d\'évolution en français médiéval. Observations d\'après quelques textes de savoir', ref:'Paris, L\'Harmattan.', doi:'' },
-  { id:4, cat:'direction', authors:'Paccosi T., Geylikman Z., Passarotti M., Bozia E. (eds.)', year:'en prép.', title:'Open Data for Quantitative Diachronic Linguistics', ref:'Journal of Open Humanities Data.', doi:'', badges:['prep'] },
+  { id:4, cat:'direction', authors:'Paccosi T., Geylikman Z., Passarotti M., Bozia E. (eds.)', year:'en prép.', title:'Open Data for Quantitative Diachronic Linguistics', ref:'<a href="https://openhumanitiesdata.metajnl.com/collections/Quantitative-Diachronic" target="_blank">Journal of Open Humanities Data.</a>', doi:'', badges:['prep'] },
 
   // CHAPITRES
   { id:5, cat:'chapitre', authors:'Geylikman Z. & Parussa G.', year:'en prép.', title:'L\'évolution du système d\'adresse dans la diachronie du français : pronoms et termes d\'adresse', ref:'Dans un volume sur la pragmatique historique du français. Paris, Honoré Champion.', doi:'', badges:['prep'] },
   { id:6, cat:'chapitre', authors:'Geylikman Z.', year:'accepté', title:'Stop talking as a bourgeois, you are a nobleman: people-related stereotyped comparisons in Medieval French (9th-15th century)', ref:'Dans <em>Negotiating Norms: Generics and stereotypes across contexts</em>, De Gruyter Mouton.', doi:'', badges:['accepted'] },
   { id:7, cat:'chapitre', authors:'Geylikman Z.', year:'accepté', title:'Nommer l\'humain dans l\'écriture scientifique en français médiéval : comment et pourquoi ? Le cas du Livre des Météores', ref:'Dans <em>Mélanges en l\'honneur de Joëlle Ducos</em>. Paris, Honoré Champion.', doi:'', badges:['accepted'] },
-  { id:8, cat:'chapitre', authors:'Geylikman Z.', year:'à paraître', title:'Le stéréotype nominal lié avec baron et son destin dans la chanson de geste en français médiéval', ref:'Dans <em>Naissance et vie des stéréotypes</em>. Rennes, PUR.', doi:'', badges:['prep'] },
+  { id:8, cat:'chapitre', authors:'Geylikman Z.', year:'à paraître', title:'Le stéréotype nominal lié avec baron et son destin dans la chanson de geste en français médiéval', ref:'Dans <em>Naissance et vie des stéréotypes</em>. Rennes, PUR.', doi:'', badges:['press'] },
   { id:9, cat:'chapitre', authors:'Boudes Y. & Geylikman Z.', year:'2019', title:'Introduction', ref:'Dans Boudes/Geylikman/Lambert/Sanchez (éds.), <em>Rythmes d\'évolution en français médiéval. Textes de savoir</em>. Paris, L\'Harmattan, p. 11-20.', doi:'' },
   { id:10, cat:'chapitre', authors:'Geylikman Z.', year:'2019', title:'Tu parli como ber : le destin de la forme monosyllabique du substantif baron dans la Geste francor', ref:'Dans Dufter/Grübl/Scharinger (éds.), <em>Des parlers d\'oïl à la francophonie</em>. De Gruyter, p. 19-43.', doi:'10.1515/9783110541816-002' },
   { id:11, cat:'chapitre', authors:'Geylikman Z.', year:'2016', title:'Le sémantisme de chevalier en français médiéval : évolution ou variation entre les genres ?', ref:'Dans Geylikman/Lambert (éds.), <em>Les rythmes d\'évolution en français médiéval. Textes littéraires</em>. Paris, L\'Harmattan, p. 53-172.', doi:'' },
 
   // ARTICLES
-  { id:12, cat:'article', authors:'Geylikman Z.', year:'2025', title:'The Emergence of personne as a Negative Indefinite in Medieval and Pre-Classical French', ref:'<em>Journal of French Language Studies</em>.', doi:'' },
+  { id:12, cat:'article', authors:'Geylikman Z.', year:'2025', title:'The Emergence of personne as a Negative Indefinite in Medieval and Pre-Classical French', ref:'<em>Journal of French Language Studies</em>.', doi:'10.1017/S0959269525100070' },
   { id:13, cat:'article', authors:'Geylikman Z.', year:'2025', title:'Nul homme en français médiéval : une grammaticalisation qui n\'a jamais eu lieu', ref:'<em>Zeitschrift für romanische Philologie</em>, 141-1.', doi:'10.1515/zrp-2025-0011' },
   { id:14, cat:'article', authors:'Geylikman Z.', year:'2024', title:'Nom d\'humain général personne en français médiéval : l\'interface sociale de l\'être humain', ref:'<em>l\'Information Grammaticale</em>, 180, p. 17-25.', doi:'10.2143/IG.180.0.3292822' },
   { id:15, cat:'article', authors:'Geylikman Z.', year:'2022', title:'La norme d\'adresse orale et écrite en français médiéval sur l\'exemple de baron et chevalier', ref:'<em>Linguistique de l\'écrit</em>, numéro thématique 3.', doi:'10.19079/lde.2022.s3.8' },
@@ -46,12 +46,15 @@ const defaultPubs = [
   { id:17, cat:'actes', authors:'Geylikman Z.', year:'2018', title:'Bachelier léger en français médiéval : une étude sémantique du figement', ref:'Dans <em>Atti del XXVIII Congresso internazionale di linguistica e filologia romanza</em>, vol. 1, p. 706-718.', doi:'' },
   { id:18, cat:'actes', authors:'Geylikman Z.', year:'2017', title:'Le sémantisme large dans la Chanson de Roland : le cas du substantif baron', ref:'Dans François/Ridruejo/Siller-Runggaldier (éds.), <em>Actes du XXVII CILPR</em>. Nancy, p. 83-94.', doi:'' },
 
+  // COMPTES-RENDUS
+  { id:28, cat:'comptes-rendus', authors:'Geylikman Z.', year:'accepté', title:'Valentine Eugène 2025. Le Roman de Renart et les péchés de langue. Droz', ref:'Studia Linguistica Romanica', doi:''},
+
   // PROGRAMME
   { id:19, cat:'programme', authors:'Geylikman Z.', year:'2025', title:'Human Nouns and Social CHANGE: a case study of Medieval French (9th-15th century)', ref:'Programme de recherche soumis au financement.', doi:'10.5281/zenodo.15127027' },
 
   // COMMUNICATIONS (sélection des plus récentes)
-  { id:26, cat:'communication', authors:'Geylikman Z.', year:'2026', title:'Stabilité sémantique en diachronie : le cas des noms d\'humain en français médiéval', ref:'DIACHRO VI, Université Sorbonne Nouvelle, juin 2026.', doi:'', badges:['avenir'], badgeLabels:{avenir:'à venir'} },
   { id:27, cat:'communication', authors:'Geylikman Z.', year:'2026', title:'Modeling human nouns\' lexicon in diachrony: a methodological essay', ref:'Lexiques/Lexicon/Lexik, ATILF, Nancy, France, décembre 2026.', doi:'', badges:['avenir'], badgeLabels:{avenir:'à venir'} },
+  { id:26, cat:'communication', authors:'Geylikman Z.', year:'2026', title:'Stabilité sémantique en diachronie : le cas des noms d\'humain en français médiéval', ref:'DIACHRO VI, Université Sorbonne Nouvelle, juin 2026.', doi:'', badges:['avenir'], badgeLabels:{avenir:'à venir'} },
   { id:20, cat:'communication', authors:'Geylikman Z.', year:'2026', title:'Counting butterflies\' matters: lexical frequencies and agency of power. The case of human nouns in Medieval French', ref:'Quantitative Diachronic Linguistics and Cultural Analytics, King\'s College London, 15-16 janv. 2026.', doi:'' },
   { id:21, cat:'communication', authors:'Geylikman Z.', year:'2025', title:'Stop talking as a bourgeois, you are a nobleman: people-related stereotyped comparisons in Medieval French', ref:'Generics and stereotypes, Leiden University, 5-6 juin 2025.', doi:'' },
   { id:24, cat:'communication', authors:'Geylikman Z.', year:'2025', title:'La norme d\'adresse et sa transgression dans quelques textes en français médiéval', ref:'Séminaire de Master de G. Parussa (invitation).', doi:'' },
